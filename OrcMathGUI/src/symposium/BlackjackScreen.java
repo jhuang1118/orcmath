969929ed3e89ee913baedc7acca126cc70ba7032
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2018-04-25 15:20:36 -0400

    changes

--- OrcMathGUI/src/symposium/BlackjackScreen.java
+++ OrcMathGUI/src/symposium/BlackjackScreen.java
@@ -16,7 +16,7 @@ public class BlackjackScreen extends FullFunctionScreen {
          Check if user has blackjack (if so, game ends)
          User draws cards (if user goes over 21, game ends)
          Dealer draws cards
-         Check for winner
+         DONE: Check for winner
 	 */
 	
 	/**
@@ -26,9 +26,11 @@ public class BlackjackScreen extends FullFunctionScreen {
 	public static final int ACE = 1, JACK = 11, QUEEN = 12, KING = 13;
 	public static final String[] CARD_RANKS = {"JOKER", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 	public static final String[] CARD_SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
+	public static final int[] CARD_VALUES = {1,2,3,4,5,6,7,8,9,10,10,10,10};
 	public ArrayList<Card> deck;
 	public static final int CLUB = 0, DIAMOND = 1, HEART = 2, SPADE = 3;
 	public ArrayList<Card> pHand;
+	public boolean isPlayerTurn;
 	public ArrayList<Card> dHand;
 
 	public BlackjackScreen(int width, int height) {
@@ -37,6 +39,7 @@ public class BlackjackScreen extends FullFunctionScreen {
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
+		isPlayerTurn = true;
 		pHand = new ArrayList<Card>();
 		dHand = new ArrayList<Card>();
 		deck = new ArrayList<Card>();
@@ -46,17 +49,28 @@ public class BlackjackScreen extends FullFunctionScreen {
 		dealInitHand(deck,dHand,2);
 		//make function later to show the card images
 		
+		
+		
+		
+		
+		/*if(winner()) {
+			System.out.println("Yay! You won the round.");
+		}
+		else
+			System.out.println("Oh no! You lost the round");*/
 	}
 	
 	public ArrayList<Card> generateRegDeck(ArrayList<Card> cardArr) {
 		deck = cardArr;
 		
 		for (int r = ACE; r <= KING; r++) {
-			for (int s = CLUB; s <= SPADE; s++){
-			
-				Card card = new Card(r,s,r + "-" + s + ".png");
+			for(int s = 0; s <= 13; s++) {
+				for (int t = CLUB; s <= SPADE; s++){
 				
-				cardArr.add(card);
+					Card card = new Card(r,t,r + "-" + t + ".png",s);
+					
+					cardArr.add(card);
+				}
 			}
 		}	
 		return cardArr;
@@ -95,18 +109,40 @@ public class BlackjackScreen extends FullFunctionScreen {
 		return cardArr;
 	}
 	
-	public void deal(ArrayList<Card> d, ArrayList<Card> hand) {
-		
+	public void deal(ArrayList<Card> hand) {
+		hand.add(deck.get(0));
+		deck.remove(0);
+		if(checkBust(hand)) {
+			System.out.println("You Lost!");
+		}
 	}
-	
-	public void hit() {
-		
+	public boolean checkBust(ArrayList<Card> hand) {
+		return countSum(hand) > 21;
+	}
+	public void hit(ArrayList<Card> hand) {
+		if(countSum(hand) <= 21) {
+			deal(hand);
+		}
 	}
 	
 	public void stand() {
-		
+		if(isPlayerTurn) {
+			isPlayerTurn = false;
+		}
+		else
+			isPlayerTurn = true;
+	}
+	public int countSum(ArrayList<Card> hand) {
+		int sum = 0;
+		for(int i = 0; i < hand.size(); i++) {
+			sum += hand.get(i).getValue();
+		}
+		return sum;
 	}
 	
+	public boolean winner() {
+		return countSum(pHand) > countSum(dHand);
+	}
 	
 	public void dealInitHand(ArrayList<Card> d, ArrayList<Card> hand, int initSize) {
 		for(int i = 0; i < initSize; i++) {
--- OrcMathGUI/src/symposium/Card.java
+++ OrcMathGUI/src/symposium/Card.java
@@ -7,11 +7,45 @@ public class Card {
 	private int rank;
 	private int suit;
 	private Graphic cardImg;
+	private int value;
 
-	public Card(int rank, int suit, String cardImg) {
+	public Card(int rank, int suit, String cardImg, int value) {
 		this.rank = rank;
 		this.suit = suit;
 		this.cardImg = new Graphic(0,0,100,100,"cardImages/" + rank + suit + ".png");
+		this.value = value;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public int getSuit() {
+		return suit;
+	}
+
+	public void setSuit(int suit) {
+		this.suit = suit;
+	}
+
+	public Graphic getCardImg() {
+		return cardImg;
+	}
+
+	public void setCardImg(Graphic cardImg) {
+		this.cardImg = cardImg;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
 	}
 	
 }
